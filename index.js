6de42b142a8bab8e14c502b72565900835dbd9cd
Rendey <rendey28@gmail.com>
2023-04-30 22:02:11 -0400

    Adicion de la funcionalidad para desencriptar un texto

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 function encriptar() {
     let texto= document.getElementById("texto").value;
     let tituloMensaje= document.getElementById("titulo-mensaje");
-   let parrafo = document.getElementById("parrafo");
-   let muneco= document.getElementById("muneco");
+    let parrafo = document.getElementById("parrafo");
+    let muneco= document.getElementById("muneco");
 
 
     let textoCifrado= texto
@@ -24,5 +24,33 @@ else {
     tituloMensaje.textContent= "Ningun mensaje fue encontrado";
     parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
     alert("Debes ingresar algun texto");
+    }
+}
+
+function desencriptar () {
+    let texto= document.getElementById("texto").value;
+    let tituloMensaje= document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let muneco= document.getElementById("muneco");
+
+    let textoCifrado= texto
+    .replace(/enter/gi, "e")
+    .replace(/imes/gi, "i")
+    .replace(/ai/gi,"a")
+    .replace(/ober/gi,"o")
+    .replace(/ufat/gi,"u");
+
+    if (texto.length != 0){
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto desencriptado con exito";
+        parrafo.textContent= "";
+        muneco.src = "./img/desencriptado.jpg";
+    }
+
+    else {
+        muneco.src= "./img/muneco.png";
+        tituloMensaje.textContent= "Ningun mensaje fue encontrado";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+        alert("Debes ingresar algun texto");
     }
 }
